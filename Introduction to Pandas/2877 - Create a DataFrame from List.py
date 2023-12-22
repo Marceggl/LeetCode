e3abbba509517efbda2c54a1bba475f2c0346d18
marceggl <62616163+Marceggl@users.noreply.github.com>
2023-12-22 12:06:04 -0300

    coments

--- Introduction to Pandas/2877 - Create a DataFrame from List.py
+++ Introduction to Pandas/2877 - Create a DataFrame from List.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from typing import List  # No LeetCode funciona sem este import
 
+"""
+1 - Esta função recebe uma lista de listas de inteiros como argumento.
+2 - Retorna um objeto DataFrame do pandas.
+3 - Os rótulos das colunas do DataFrame são definidos como "Coluna 1" e "Coluna 2".
+"""
+
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     df = pd.DataFrame(student_data, columns=["student_id", "age"])
@@ -8,6 +14,9 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     return df
 
 
+""""
+Chama a função CreateDataframe enviando os dados diretamente pelo chamado
+"""
 dados_df = createDataframe(student_data=[[1, 15], [2, 11], [3, 11], [4, 20]])
 
 print(dados_df)
